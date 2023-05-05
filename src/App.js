c07a8d6051f2cd3ecedb1dba2df64e3611eb3449
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-05 23:26:11 +0530

    a basic score counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react'
+import React from 'react'
+import "./App.css"
 function App() {
+  const [scoreA, setscoreA] = useState(0);
+  const [scoreB, setscoreB] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container '>
+      <h1 className="text-center mb-5 mt-5">Cricket Score</h1>
+      <div className="row">
+        <div className="col-sm-6 mb-3 mb-sm-0">
+          <div className="card ">
+            <div className="card-body">
+              <h1 className="card-title text-center ">Team A</h1>
+              <h3 className='text-center '>{scoreA}</h3>
+              <div className="text-center">
+                <button type="button" className="btn btn-success mx-5" onClick={() => {
+                  setscoreA(scoreA + 1)
+                }}>+</button>
+                <button type="button" className="btn btn-danger mx-5" onClick={() => {
+                  if (scoreA == 0) {
+                    alert("Score cant be Negative")
+                  }
+                  else {
+
+                    setscoreA(scoreA - 1)
+                  }
+                }}>-</button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="card-title text-center ">Team B</h1>
+              <h3 className='text-center '>{scoreB}</h3>
+              <div className="text-center">
+                <button type="button" className="btn btn-success  mx-5" onClick={() => {
+                  setscoreB(scoreB + 1)
+                }}>+</button>
+                <button type="button" className="btn btn-danger  mx-5" onClick={() => {
+                  if (scoreB == 0) {
+                    alert("Score cant be Negative")
+                  }
+                  else {
+
+                    setscoreB(scoreB - 1)
+                  }
+                }}>-</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
