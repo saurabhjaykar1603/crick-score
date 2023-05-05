24af03545ac454425f2b276a0c55a97be90c38a6
saurabhjaykar1602 <jaykarsaurabh97@gmail.com>
2023-05-06 00:37:48 +0530

    card midified with css

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ function App() {
   const [scoreA, setscoreA] = useState(0);
   const [scoreB, setscoreB] = useState(0);
   return (
-    <div className='container '>
-      <h1 className="text-center mb-5 mt-5">Cricket Score</h1>
+    <div className='container container-bg '>
+      <h1 className="text-center mb-5 mt-5">Cricket Score 🏏</h1>
       <div className="row">
         <div className="col-sm-6 mb-3 mb-sm-0">
-          <div className="card ">
+          <div className="card card-bg">
             <div className="card-body">
-              <h1 className="card-title text-center ">Team A</h1>
-              <h3 className='text-center '>{scoreA}</h3>
+              <h1 className="card-title text-center text-color">Team A</h1>
+              <h3 className='text-center text-color'>{scoreA}</h3>
               <div className="text-center">
-                <button type="button" className="btn btn-success mx-5" onClick={() => {
+                <button type="button" className="btn btn-success btn-lg mx-5 " onClick={() => {
                   setscoreA(scoreA + 1)
                 }}>+</button>
-                <button type="button" className="btn btn-danger mx-5" onClick={() => {
+                <button type="button" className="btn btn-danger btn-lg mx-5 " onClick={() => {
                   if (scoreA == 0) {
                     alert("Score cant be Negative")
                   }
@@ -27,20 +27,19 @@ function App() {
                   }
                 }}>-</button>
               </div>
-
             </div>
           </div>
         </div>
         <div className="col-sm-6">
-          <div className="card">
+          <div className="card card-bg">
             <div className="card-body">
-              <h1 className="card-title text-center ">Team B</h1>
-              <h3 className='text-center '>{scoreB}</h3>
+              <h1 className="card-title text-center text-color ">Team B</h1>
+              <h3 className='text-center text-color '>{scoreB}</h3>
               <div className="text-center">
-                <button type="button" className="btn btn-success  mx-5" onClick={() => {
+                <button type="button" className="btn btn-success btn-lg  mx-5 " onClick={() => {
                   setscoreB(scoreB + 1)
                 }}>+</button>
-                <button type="button" className="btn btn-danger  mx-5" onClick={() => {
+                <button type="button" className="btn btn-danger btn-lg  mx-5 " onClick={() => {
                   if (scoreB == 0) {
                     alert("Score cant be Negative")
                   }
